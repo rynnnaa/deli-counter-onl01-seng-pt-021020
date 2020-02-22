7600ba24da0c0d5b48e2ed1bc5c179cc98c9c793
Ryna Truong <rynatruong@outlook.com>
2020-02-22 19:31:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 def line(array)
-  if array.length == 0 
+  if array.length == 0
     puts "The line is currently empty."
   else
     phrase_arr = array.each_with_index.map do |name, idx|
-    "#{idx + 1}, #{name}"
+      "#{idx + 1}, #{name}"
+    end
+    puts "The line is currently: #{phrase_arr.join(',')}"
   end
-  puts "The line is currently: #{phrase_arr.join(',')}"
-end
 
 def take_a_number(katz_array, name_of_person_joining)
   katz_array << name_of_person_joining
-  outs "Welcome, #{name_of_person_joining}. You are number #{katz_array.length} in line."
+  puts "Welcome, #{name_of_person_joining}. You are number #{katz_array.length} in line."
 end
