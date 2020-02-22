daf6774c559bbcf9af396586e400be6c50e5c883
Ryna Truong <rynatruong@outlook.com>
2020-02-22 19:32:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(array)
     puts "The line is currently: #{phrase_arr.join(',')}"
   end
 
-def take_a_number(katz_array, name_of_person_joining)
-  katz_array << name_of_person_joining
-  puts "Welcome, #{name_of_person_joining}. You are number #{katz_array.length} in line."
-end
+# def take_a_number(katz_array, name_of_person_joining)
+#   katz_array << name_of_person_joining
+#   puts "Welcome, #{name_of_person_joining}. You are number #{katz_array.length} in line."
+# end
