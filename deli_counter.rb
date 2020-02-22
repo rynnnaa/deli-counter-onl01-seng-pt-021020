b0875acc21abcd798b03c75de11ea50b72369119
Ryna Truong <rynatruong@outlook.com>
2020-02-22 19:29:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,19 +7,8 @@ def line(array)
   end
   puts "The line is currently: #{phrase_arr.join(',')}"
 end
-end
 
 def take_a_number(katz_array, name_of_person_joining)
-  counter = 0
-  number = katz_arrray.length
-  if number == 0 
-    katz_array.unshift(name_of_person_joining)
-  else 
-    array.push(name_of_person_joining)
-    puts "Welcome, #{name_of_person_joining}. You are number #{number} in line." 
-  end
-    katz_deli.each do |name|
-      puts "Welcome, #{name}. You are number #{counter += 1} in line."
-    end
-  end
+  katz_array << name_of_person_joining
+  outs "Welcome, #{name_of_person_joining}. You are number #{katz_array.length} in line."
 end
