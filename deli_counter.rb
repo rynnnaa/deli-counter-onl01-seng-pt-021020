2f4131dfb28a6747ff0b15c397a836605d42c802
Ryna Truong <rynatruong@outlook.com>
2020-02-22 19:42:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def now_serving(arr)
   if arr.length == 0 
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{arr.pop}."
+    puts "Currently serving #{arr.shift}."
   end
 end
   
