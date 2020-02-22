db1d47b36d23eda03a15a340f152ce3876ed84ff
Ryna Truong <rynatruong@outlook.com>
2020-02-22 19:15:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(array)
   str = "The line is currently:"
   if array.length == 0 
-    puts "The line is currently empty."
+    puts str
     elsif array.length > 0
     arr.each_with_index do |name, idx|
       
