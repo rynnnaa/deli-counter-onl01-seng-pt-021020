478e5d3f5f3c6576d29a317616a9449ab2eb5c90
Ryna Truong <rynatruong@outlook.com>
2020-02-22 19:27:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(array)
     phrase_arr = array.each_with_index.map do |name, idx|
     "#{idx + 1}, #{name}"
   end
-  puts "The line is currently: #{phrase_arr.join(,)}"
+  puts "The line is currently: #{phrase_arr.join(',')}"
+end
 end
 
 def take_a_number(katz_array, name_of_person_joining)
