9ee7e4eceda9238ec933813172aa8b2323a4556a
Ryna Truong <rynatruong@outlook.com>
2020-02-22 19:26:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,10 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else
-    phrase arr = array.each_with_index.map do |name, idx|
+    phrase_arr = array.each_with_index.map do |name, idx|
     "#{idx + 1}, #{name}"
   end
+  puts "The line is currently: #{phrase_arr.join(,)}"
 end
 
 def take_a_number(katz_array, name_of_person_joining)
