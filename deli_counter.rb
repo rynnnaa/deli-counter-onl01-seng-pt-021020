d9953efb9f979db768aa374ef032c1ae9a370fde
Ryna Truong <rynatruong@outlook.com>
2020-02-22 19:13:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(array)
+  str = "The line is currently:"
+  if array.length == 0 
+    puts "The line is currently empty."
+    elsif array.length > 0
+    arr.each_with_index do |name, idx|
+      
+    puts "The line is currently:"
+  end
+end
+
+def take_a_number(katz_array, name_of_person_joining)
+  counter = 0
+  number = katz_arrray.length
+  if number == 0 
+    katz_array.unshift(name_of_person_joining)
+  else 
+    array.push(name_of_person_joining)
+    puts "Welcome, #{name_of_person_joining}. You are number #{number} in line." 
+  end
+    katz_deli.each do |name|
+      puts "Welcome, #{name}. You are number #{counter += 1} in line."
+  end
+end
